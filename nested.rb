42c7fb33d5d414d37d55055a3d77175b87020dc6
Richard Chen <richard.c.chen@gmail.com>
2018-07-29 00:42:18 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -81,10 +81,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto]
-programmer_hash[:yukihiro_matsumoto[:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages] = ["Lisp", "C"]
-programmer_hash
+#programmer_hash[:yukihiro_matsumoto]
+#programmer_hash[:yukihiro_matsumoto[:known_for] = "Ruby"
+#programmer_hash[:yukihiro_matsumoto][:languages] = ["Lisp", "C"]
+#programmer_hash
     
 end
 
